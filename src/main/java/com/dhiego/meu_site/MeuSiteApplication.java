c31706281ddc5ec76085c3e5cd3ea22af1e785e0
dhiego <dhiego12052006@gmail.com>
2025-04-10 23:24:31 -0300

    add: Aplicação principal

---
+++ src/main/java/com/dhiego/meu_site/MeuSiteApplication.java
@@ -0,0 +1,13 @@
+package com.dhiego.meu_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuSiteApplication.class, args);
+	}
+
+}
