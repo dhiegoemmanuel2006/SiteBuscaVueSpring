b56fcdcfac77c25288429f1ca97eb1a715eef185
Dhiego Andrade <160744891+dhiegoemmanuel2006@users.noreply.github.com>
2025-04-13 12:07:03 -0300

    update: annotations para a configuração do SWAGGER

--- src/main/java/com/dhiego/meu_site/controller/BuscaController.java
+++ src/main/java/com/dhiego/meu_site/controller/BuscaController.java
@@ -4,6 +4,8 @@ import com.dhiego.meu_site.dto.OperadorasAtivasDTO;
 import com.dhiego.meu_site.services.OperadorasativaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -11,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-@Tag(name = "OperadorasAtivas", description = "Operação de busca das operadoraas ativas")
+@Tag(name = "OperadorasAtivas", description = "Operação de busca das operadoras ativas")
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin
@@ -23,12 +25,23 @@ public class BuscaController {
             description = "Retorna 10 Operadoras Ativas com base nos dados da ANS",
             tags = {"OperadorasAtivas"},
             responses = {
-                    @ApiResponse(responseCode = "200", description = "Operadoras encontradas"),
-                    @ApiResponse(responseCode = "204", description = "Nenhuma operadora encontrada")
+                    @ApiResponse(responseCode = "200",
+                            description = "Operadoras encontradas",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    examples = @ExampleObject(
+                                            value = "[{\"nome\": \"Unimed Rio Verde\", \"registroANS\": \"123456\", " +
+                                                    "\"cnpj\": \"12345678901234\", \"cidade\": \"Rio Verde\", \"UF\": \"GO\"}]"
+                                    )
+                            )
+                    ),
+                    @ApiResponse(responseCode = "400", description = "Termo de busca inválido"),
+                    @ApiResponse(responseCode = "204", description = "Nenhuma operadora encontrada"),
+                    @ApiResponse(responseCode = "500", description = "Erro interno no servidor / banco de dados")
             }
     )
     @PostMapping("/busca")
-    public ResponseEntity<List<OperadorasAtivasDTO>> buscar(@Parameter(description = "O termo que fará a busca", required = true, example = "unimed") @RequestBody String termo){
+    public ResponseEntity<List<OperadorasAtivasDTO>> buscar(@Parameter(description = "O termo que fará a busca", required = true, example = "{\"termo\":\"unimed\"}") @RequestParam String termo){
         List<OperadorasAtivasDTO> resultados = oaService.buscarOperadoras(termo);
 
         if (resultados.isEmpty()) {
