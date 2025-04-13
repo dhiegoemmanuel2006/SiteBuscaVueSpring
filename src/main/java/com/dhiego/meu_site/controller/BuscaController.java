a37677eb06e2ce61a040135e5d2df89092108a5e
Dhiego Andrade <160744891+dhiegoemmanuel2006@users.noreply.github.com>
2025-04-12 22:21:21 -0300

    add: adicionando annotations para que o swagger consiga documentar a API da melhor maneira

--- src/main/java/com/dhiego/meu_site/controller/BuscaController.java
+++ src/main/java/com/dhiego/meu_site/controller/BuscaController.java
@@ -2,19 +2,34 @@ package com.dhiego.meu_site.controller;
 
 import com.dhiego.meu_site.dto.OperadorasAtivasDTO;
 import com.dhiego.meu_site.services.OperadorasativaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Tag(name = "OperadorasAtivas", description = "Operação de busca das operadoraas ativas")
 @RequiredArgsConstructor
 @RestController
 @CrossOrigin
 public class BuscaController {
     private final OperadorasativaService oaService;
 
+
+    @Operation(
+            summary = "Obter 10 usuário pelo Nome ou Razão social",
+            description = "Retorna 10 Operadoras Ativas com base nos dados da ANS",
+            tags = {"OperadorasAtivas"},
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Operadoras encontradas"),
+                    @ApiResponse(responseCode = "204", description = "Nenhuma operadora encontrada")
+            }
+    )
     @PostMapping("/busca")
-    public ResponseEntity<?> buscar(@RequestBody String termo){
+    public ResponseEntity<List<OperadorasAtivasDTO>> buscar(@Parameter(description = "O termo que fará a busca", required = true, example = "unimed") @RequestBody String termo){
         List<OperadorasAtivasDTO> resultados = oaService.buscarOperadoras(termo);
         if (resultados.isEmpty()) {
             return ResponseEntity.noContent().build();
--- src/main/java/com/dhiego/meu_site/dto/OperadorasAtivasDTO.java
+++ src/main/java/com/dhiego/meu_site/dto/OperadorasAtivasDTO.java
@@ -1,6 +1,7 @@
 package com.dhiego.meu_site.dto;
 
 import com.dhiego.meu_site.database.entity.Operadorasativa;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +10,19 @@ import lombok.RequiredArgsConstructor;
 @AllArgsConstructor
 @Data
 public class OperadorasAtivasDTO {
+    @Schema(description = "Razão Social da operadora", example = "unimed rio verde")
     private String nome;
+
+    @Schema(description = "Registro ANS da operadora", example = "123456789",  pattern = "^\\\\d{14}$")
     private String registroANS;
+
+    @Schema(description = "CNPJ da operadora", example = "12345678910", pattern = "^\\d+$")
     private String cnpj;
+
+    @Schema(description = "Cidade onde a operadora fica", example = "São Paulo")
     private String cidade;
+
+    @Schema(description = "UF onde a operadora fica", example = "SP")
     private String UF;
 
     public OperadorasAtivasDTO(Operadorasativa oa) {
